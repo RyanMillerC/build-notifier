66e90bb474c074ca6b40819e8c656fe69fd2b477
Ryan Miller <ryan@devopsmachine.com>
2018-10-15 14:32:49 -0400

    Add JSDocs to JavaScript files

--- handler.js
+++ handler.js
@@ -1,4 +1,7 @@
-// handler.js
+/**
+ * NodeJS handler using @probot/serverless-lambda to wrap Probot
+ * for AWS Lambda.
+ **/
 
 const serverlessLambda = require('@probot/serverless-lambda')
 const appFn = require('./')
