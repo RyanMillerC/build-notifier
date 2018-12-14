d1e34a4d77b10fb0e78fa873ee75601b09c8fd3b
Ryan Miller <ryan@devopsmachine.com>
2018-12-14 07:30:13 -0500

    Update JSDoc with new text from README

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 /**
  * build-notifier
  *
- * GitHub bot to comment on new pull requests. Bot will comment with
- * a predefinded message including a URL to a build of the requested
- * PR. This assumes that all PRs are being deployed to a specific
- * URL under a resource name matching the PR branch name. For example,
- * a link to, `http://devopsmachine.com/PRs/add-spacing-to-navbar`,
- * would be added to the bot's comment on a PR to merge the branch,
- * `add-spacing-to-navbar`, into master. In this example, `http://dev-
- * opsmachine.com/PRs`, is where all builds are being deployed, each
- * under a seperate resource suffix matching the branch name.
+ * Build-notifier is a GitHub [Probot](https://github.com/probot/probot) App to comment on new pull requests. This bot will comment with a predefinded message including a URL to a build of the requested PR. This assumes that all PRs are being deployed to a specific URL for testing under a resource name matching the PR branch name. For example, a link to, `http://devopsmachine.com/PRs/add-spacing-to-navbar`, would be added to the bot's comment on a PR to merge the branch, `add-spacing-to-navbar`, into master. In this example, `http://devopsmachine.com/PRs`, is where all builds are being deployed, each under a seperate resource suffix matching the branch name.
  *
- * The notification message and deployment location are configured
- * in, `.github/build-notifier.yml`, on the target repo. There are
- * two values that must be set in the configuration file:
+ * The notification message and deployment location are configured in, `.github/build-notifier.yml`, on the target repo. There are two values that must be set in the configuration file:
  *
- *   `deploymentURL` - Base URL where builds will be deployed. In the
- *        example above this is "http://devopsmachine.com/PRs".
- *   `notifyMessage` - Bot comment message. This can be formatted
- *        with any Markdown that is supported in PR comments. The text,
- *        `{{LINK}}`, with be replaced with a link to the PR build.
+ * + **deploymentURL** - Base URL where builds will be deployed. In the example above this is `http://devopsmachine.com/PRs`.
+ * + **notifyMessage** - Bot comment message. This can be formatted with any Markdown that is supported in PR comments. The text, `{{LINK}}`, with be replaced with a link to the PR build.
  *
- * @summary GitHub bot to comment on new pull requests with a link to
- *          a live demo of the requested PR.
+ * @summary GitHub bot to comment on new pull requests with a link to a live demo of the requested PR.
  *     @see https://github.com/RyanMillerC/build-notifier/README.md
  **/
 
